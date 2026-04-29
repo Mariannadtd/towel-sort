dc1c51658990b1821884a67d5b358c78e2915c42
Mariannadtd <romanova.lenovo@gmail.com>
2026-04-29 11:15:41 +0300

    solve towel sort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (!matrix) {
+    return result;
+  }
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      for (let k = 0; k < matrix[i].length; k += 1) {
+        result.push(matrix[i][k]);
+      }
+    } else {
+      for (let j = matrix[i].length - 1; j >= 0; j -= 1) {
+        result.push(matrix[i][j]);
+      }
+    }
+  }
+
+  return result;
 };
